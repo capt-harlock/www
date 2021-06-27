76bceacaaa374ef46362d7cd8fac9cbe1c9849c9
luca <luca@node1.home>
2021-06-27 20:27:56 +0200

    First commit

---
+++ src/Home.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+export default () => (
+    <article>
+        <h2>Hello! I'm Luca Marrocco 🚀</h2>
+        <p>I'm a professional software developer with 20 years of experience. Java / React / IOS</p>
+        <p>I'd to mention tha I'm a drake to run and manage Linux production systems, with particular regards to Ubuntu.</p>
+        <br/>
+        <p>Since Jun 2021 I decided to invest my time and money in Polkadot, because</p>
+        <p>1. Polkadot Vision, blockchain is here to stay, plurality of chain that coexists and cooperate together.</p>
+        <p>2. Polkadot Technology, it is build in Rust for backend / core system and use React for frontend application.</p>
+        <p>3. Polkadot Community, open, active, polite and proactive super skilled people who believe in this project.</p>
+        <br/>
+        <p>To accomplish my next step to promote my nodes as official validator, I need help. Your rewards would be proportional to your investment.</p>
+        <br/>
+        <h2>Commitment</h2>
+        <p>0% Fee until this first polkadot / kusama node become an official validator</p>
+        <p>Install and keep update all nodes as soon as new polkadot release are published</p>
+        <p>Build and make public available tools and procedure to automatize running polkadot node in production</p>
+        <p>Test, bugfix and improve current polkadot application ecosystem, including but not limited to substrate core</p>
+        <br/>
+        <h2>Roadmap</h2>
+        <p><input type='checkbox' checked='true'/>1. <strong>DONE</strong> Run 1 Kusama node</p>
+        <p><input type='checkbox' checked='true'/>2. <strong>DONE</strong> Run 1 Polkadot node</p>
+        <p><input type='checkbox' checked='true'/>3. <strong>DONE</strong> Apply for 1k Thousand Validator</p>
+        <p><input type='checkbox' disabled='true'/>4. <strong>WORK</strong> Promoted as Kusama Validator</p>
+        <p><input type='checkbox' disabled='true'/>5. <strong>NEXT</strong> Promoted as Polkadot Validator</p>
+        <p><input type='checkbox' disabled='true'/>6. <strong>NEXT</strong> Run 2 Polkadot / Kusama validators x 1 regions (CH)</p>
+        <p><input type='checkbox' disabled='true'/>7. <strong>NEXT</strong> Run 4 Polkadot / Kusama validators x 2 regions (CH, FR)</p>
+        <p><input type='checkbox' disabled='true'/>8. <strong>NEXT</strong> Run 6 Polkadot / Kusama validators x 3 regions (CH, FR, DE)</p>
+        <p><input type='checkbox' disabled='true'/>9. <strong>NEXT</strong> to the MOON! 🌝</p>
+        <br/>
+        <p>Please contact me if you wish to have more details.</p>
+        <br/>
+        <h2>Contact</h2>
+        <p>+41 79 1 538 548</p>
+        <p>luca@capt-harlock.com</p>
+        <br/>
+        <p>Lugano | Switzerland</p>
+    </article>
+)
